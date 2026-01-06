380dc41b956fcd389e48736e3e7daf07e80d38f3
Dr SHXWN <seanmandivhei47@gmail.com>
2026-01-06 19:24:31 +0200

    Create get-messages.ts

---
+++ backend/forum/get-messages.ts
@@ -0,0 +1,10 @@
+import { KVStore } from '../storage/kv.ts';
+
+const forumStore = new KVStore("forum");
+
+export async function getMessages(threadId: string, limit = 50) {
+    const thread = await forumStore.get(threadId);
+    if(!thread) throw new Error("Thread not found");
+
+    return thread.posts.slice(-limit);
+}
